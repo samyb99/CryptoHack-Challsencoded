86b8ae9399013c789e29bc88caaf0ee010ab6c53
BONTEMPS <samy.bontemps@orange.fr>
2021-02-12 15:39:48 +0100

    First commit with encoding/xor challenges

---
+++ GENERAL/XOR/You either know, XOR you don't/eitherKnow.py
@@ -0,0 +1,4 @@
+x = "♫ ('7↓EDM←!;7↑K▼▼▬5'7‼M^]∟h-`ZV▼_Oh{c♫QA]←jy`ZR▼_Nk{`♫TNX↔h*b\U↑^→m-c[V∟_Ljy`ZR▼_Nk{`♫TNX↔h*b\R∟_Ok{gZTL]Hh{uB§§♀►6;7‼◄YPY:h1↓∟        ↓▬#h9♫∟YPY⌂h|☺
+►♥Q;' C♠Y3Y5fr
+♂M→to?K♀↨M♥1?∟  ♣∟*cr←  ↑♦↨,**▼LP↔♂1!&C♫∟¶P<*1☻♂=+rVE^JW2 ;♣M→♣♂p,r5E↕DY>  K♠UM↕x&<K▼►↔Q;6"♥ ♂AY+; E ↨♫▬<*z   G§=!∟
+        ♣∟*f{BL ▼►6;z☼ →♦       0* ♫☺P"
---
+++ README.txt
@@ -0,0 +1,2 @@
+Here it is a repository with the different challenges and categories from : https://cryptohack.org/challenges/
+But, the answer in the repository will be ciphered dependably on the challenge.
